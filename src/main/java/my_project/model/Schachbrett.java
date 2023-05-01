da956cf1c6936031b96cce3af29a951b2c255403
Mattea <rukimatt@kkg-do.eu>
2023-05-01 16:39:43 +0200

    episch

--- src/main/java/my_project/model/Schachbrett.java
+++ src/main/java/my_project/model/Schachbrett.java
@@ -11,8 +11,8 @@ public class Schachbrett extends GraphicalObject {
     public void draw(DrawTool drawTool){
         drawTool.setCurrentColor(new Color(50, 0, 50));
 
-        for(int i = 0; i*50 < Config.WINDOW_HEIGHT;i++){
-            for (int j = 0; j*50 < Config.WINDOW_WIDTH;j++){
+        for(int i = 0; i * 50 < Config.WINDOW_HEIGHT; i++){
+            for(int j = 0; j * 50 < Config.WINDOW_WIDTH; j++){
                 drawTool.drawFilledRectangle(j*100, i*100,50, 50);
                 drawTool.drawFilledRectangle(50+j*100, 50+i*100, 50, 50);
             }
